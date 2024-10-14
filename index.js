a71ecc62ded56d95fb181c4c854384dbf2d240dc
MuhammadTalal34 <talalsadaqat123@gmail.com>
2024-10-14 18:03:37 +0500

    done with index file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// set method (template engine : )
+// create application 
+const app = express();
+
+const port = 3000
+// get , post , delete , put , patch 
+app.get('/' , (req , res)=>{
+    res.end('hy')
+} )
+app.listen(port , ()=>{
+    console.log(`Listening to Port : ${port}`)
+})
